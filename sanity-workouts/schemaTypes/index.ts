82ec9e5d3ec51869e6ecbe3c8ee1e147aabde1f9
Jessica Strachan <jstrachan@rawnet.com>
2024-08-30 11:28:08 +0100

    Add Schemas for exercises, workouts, and programmes

--- sanity-workouts/schemaTypes/index.ts
+++ sanity-workouts/schemaTypes/index.ts
@@ -1,1 +1,11 @@
-export const schemaTypes = []
+import {exerciseType} from './exerciseType'
+import {workoutType} from './workoutType'
+import {intensityType} from './intensityType'
+import {programmeType} from './programmeType'
+
+export const schemaTypes = [
+  exerciseType,
+  workoutType,
+  intensityType,
+  programmeType
+]
