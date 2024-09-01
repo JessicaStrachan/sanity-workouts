94357036657602986dd482f5b0b5413580e4bc01
Jessica Strachan <jstrachan@rawnet.com>
2024-09-01 11:48:26 +0100

    Move styling - attempt to fix styles not working in deployment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Paytone_One } from "next/font/google";
-import "./styles/globals.css";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
